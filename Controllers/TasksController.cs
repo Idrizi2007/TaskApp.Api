6bdab406944b78fb6bf33ffda80da5f9f96f7d1e
idriz <idriz@DESKTOP-8F2CNJ2>
2026-02-08 23:58:38 -1100

    Add project files.

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskApp.Api.Controllers
+{
+    [ApiController]
+    [Route("tasks")]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(new List<string>());
+        }
+    }
+}
